ed584d9bb5214d0b44a2af2b4f252f02a9c5b707
Thales Maciel <thalesmsm12@gmail.com>
2022-11-15 17:17:45 -0300

    feat: validate password

--- backend/src/controllers/User.controller.ts
+++ backend/src/controllers/User.controller.ts
@@ -12,9 +12,17 @@ export default class UserController {
     const { body } = req;
     const { username, password, accountId } = body; 
 
+    const validPassword = /^(?=.*\d)(?=.*[A-Z])(?=.*[a-zA-Z]).{7,}$/;
+
+    if (!validPassword.test(password)) {
+      return res.status(401).json({ 
+        message: 'Password must be minimum eight characters, at least one uppercase letter and one number' 
+      });
+    }
+
     await this.userService.createUser(username, password, accountId);
 
-    res.status(201).json({
+    return res.status(201).json({
       message: 'User created',
       body: {
         username,
