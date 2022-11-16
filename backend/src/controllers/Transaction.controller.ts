ae3625ae773003471be8262da8579d3a8d19756c
Thales Maciel <thalesmsm12@gmail.com>
2022-11-16 12:38:04 -0300

    feat: a transaction changes accounts balance

--- backend/src/controllers/Transaction.controller.ts
+++ backend/src/controllers/Transaction.controller.ts
@@ -1,12 +1,15 @@
 import { NextFunction, Request, Response } from 'express';
 import ITransaction from '../Interfaces/ITransaction';
+import AccountService from '../services/Account.service';
 import TransactionService from '../services/Transaction.service';
 
 class TransactionController {
   transactionService: TransactionService;
+  accountService: AccountService;
 
   constructor() {
     this.transactionService = new TransactionService();
+    this.accountService = new AccountService();
   }
 
   public create = async (req: Request, res: Response, next: NextFunction) => {
@@ -17,10 +20,17 @@ class TransactionController {
       value: body.value,
       createdAt: new Date,
     };
-    console.log(transaction);
     
+    const debitedAccount = await this.accountService.getAccountById(Number(body.debitedAccountId));
+    const debitedAccountBalance = debitedAccount.balance - body.value;
+    
+    const creditedAccount = await this.accountService.getAccountById(Number(body.creditedAccountId));
+    const creditedAccountBalance = creditedAccount.balance + body.value;
+
     try {
       const newTransaction = await this.transactionService.createTransaction(transaction);
+      await this.accountService.updateAccount({...debitedAccount, balance: debitedAccountBalance}, body.debitedAccountId);
+      await this.accountService.updateAccount({...creditedAccount, balance: creditedAccountBalance}, body.creditedAccountId);
       return res.status(201).json(newTransaction);
     } catch (error) {
       next(error);
--- backend/src/services/Account.service.ts
+++ backend/src/services/Account.service.ts
@@ -1,26 +1,31 @@
 import AccountModel from '../database/models/Account.model';
+import IAccount from '../interfaces/IAccount';
 
 class AccountService {
   public accountModel = AccountModel;
 
-  public async getAllAccounts() {
+  public async getAllAccounts(): Promise<IAccount[]> {
     const accounts = await this.accountModel.findAll();
     
     return accounts;
   }
 
-  public async getAccountById(id: number) {
+  public async getAccountById(id: number): Promise<IAccount> {
     const account = await this.accountModel.findOne({ where: { id } });
+    
+    return account as IAccount;
+  }
+
+
+  public async createAccount(id: number, balance: number): Promise<IAccount> {
+    const account = await this.accountModel.create({id, balance});
     
     return account;
   }
 
-
-  public async createAccount(id: number, balance: number) {
-    
-    const account = await this.accountModel.create({id, balance});
-    
-    return account;
+  public async updateAccount(body: IAccount, id: number): Promise<Object> {
+    await this.accountModel.update(body, { where: { id } });
+    return { message: 'Transaction success' };
   }
 }
 
