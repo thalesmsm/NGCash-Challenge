7c96181601b4d94a3b57e846e6d585652515a800
Thales Maciel <thalesmsm12@gmail.com>
2022-11-19 11:30:04 -0300

    style: styles on account page

--- frontend/components/Transactions.tsx
+++ frontend/components/Transactions.tsx
@@ -87,113 +87,116 @@ const Transactions = () => {
     userLogged && creditedAccount && creditedValue && token && postTransaction(userLogged?.accountId, creditedAccount?.accountId, creditedValue, token);
   }
   return (
-    <><header>
-      <div className="flex justify-between items-center border-b-[1px] py-4">
-        <p className="text-lg">Olá, {userLogged?.username}!</p>
-        <Image src="/logo-ngcash.svg" width={90} height={90} alt="ng Logo" />
-        <div className="flex flex-col justify-between items-center h-14">
-          <p>Saldo: <span>R$ {userLogged && userLogged.account.balance.toFixed(2)}</span></p>
-          <Link href="/">
-            <button
-              className="ml-4 bg-gradient-to-br from-gray-400 to-gray-600 text-white font-bold px-4 rounded hover:bg-gradient-to-br hover:from-gray-500 hover:to-gray-700"
-              onClick={logOut}
-            >
-              Sair
-            </button>
-          </Link>
-        </div>
-      </div>
-    </header>
-    <main>
-      <section className="flex flex-col items-center mt-5">
-        <div className="flex justify-between items-center w-[100vw] mb-5">
-          <h1 className="text-xl align-middle font-bold">Transaferências</h1>
-          <select
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-zinc-400 focus:border-zinc-500 block p-2.5"
-            onChange={handleSelectTransactions}
-          >
-            <option value="todas">Todas</option>
-            <option value="cashIn">Cash in</option>
-            <option value="cashOut">Cash out</option>
-          </select>
-        </div>
-        <table className="border-collapse w-[100vw] text-center">
-          <thead>
-            <tr className="border-b-2 bg-gradient-to-b from-gray-400/30 to-gray-600/30 text-lg">
-              <th className="border-r-[1px]  p-2">Transação</th>
-              <th className="border-r-[1px]  p-2">Valor</th>
-              <th className="p-2">Data</th>
-            </tr>
-          </thead>
-          <tbody>
-            {transactions?.map((transaction) => (
-              <tr
-                key={transaction.id}
-                className="bg-gradient-to-tr to-gray-400 from-gray-600"
+    <div>
+      <header className="flex justify-between items-center border-b-[1px] py-4 px-4">
+        {/* <div> */}
+          <p className="text-lg">Olá, {userLogged?.username}!</p>
+          <Image src="/logo-ngcash.svg" width={90} height={90} alt="ng Logo" />
+          <div className="flex flex-col justify-between items-center h-14">
+            <p>Saldo: <span>R$ {userLogged && userLogged.account.balance.toFixed(2)}</span></p>
+            <Link href="/">
+              <button
+                className="ml-4 bg-gradient-to-br from-gray-400 to-gray-600 text-white font-bold px-4 rounded hover:bg-gradient-to-br hover:from-gray-500 hover:to-gray-700"
+                onClick={logOut}
               >
-                <td className="border-r-[1px] font-bold p-2">
-                  {userLogged?.accountId === transaction.debitedAccountId ? 'Cash out' : 'Cash in'}
-                </td>
-                <td className={`${userLogged?.accountId === transaction.debitedAccountId ? 'text-red-500' : 'text-green-500'} border-r-[1px] text-lg font-bold p-2`}>
-                  {`${userLogged?.accountId === transaction.debitedAccountId ?
-                      `- R$ ${transaction.value.toFixed(2)}` :
-                      ` R$ ${transaction.value.toFixed(2)}`}`}
-                </td>
-                <td className="p-2">
-                  {(transaction.createdAt.slice(0, 10).split('-').reverse().join('/'))}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </section>
-      <section>
-        <form onSubmit={handleSubmit}>
-          <div className="flex flex-col justify-between items-center w-[100vw] mb-5">
-            <h1 className="text-xl align-middle font-bold">Realizar Transferência</h1>
-            <div className="flex w-full">
-              <div className="w-1/2">
-                <label className="block text-gray-200 text-sm font-bold mb-2" htmlFor="valor">
-                  Valor
-                </label>
-                <input
-                  id="valor"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-zinc-400 focus:border-zinc-500 p-3 w-40"
-                  type="number"
-                  onChange={
-                    (e: React.FormEvent<HTMLInputElement>) => 
-                      setCreditedValue(Number(e.currentTarget.value))
-                  }
-                />
-              </div>
-              <div className="w-1/2">
-                <label className="block text-gray-200 text-sm font-bold mb-2" htmlFor="para">
-                  Para:
-                </label>
-                <select
-                  id="para"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-zinc-400 focus:border-zinc-500 block p-3 w-40"
-                  onChange={handleSelectCredited}
-                  >
-                  <option></option>
-                  {
-                    allUSers?.map((user) => {
-                      return <option key={user.accountId}>{user.username}</option>;
-                    })
-                  }
-                </select>
-              </div>
-            </div>
-            <button
-            className="ml-4 bg-gradient-to-br from-gray-400 to-gray-600 text-white font-bold px-4 py-2 rounded hover:bg-gradient-to-br hover:from-gray-500 hover:to-gray-700"
-            type="submit"
-            >
-              Ok
-            </button>
+                Sair
+              </button>
+            </Link>
           </div>
-        </form>
-      </section>
-    </main></>
+        {/* </div> */}
+      </header>
+      <main className="md:flex md:justify-around">
+        <section className="flex flex-col items-center mt-5">
+          <div className="flex justify-between items-center w-[100vw] mb-5 md:justify-center md:w-[350px] lg:w-[450px] lg:max-w-xl">
+            <h1 className="text-xl align-middle font-bold md:mr-12">Transaferências</h1>
+            <select
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-zinc-400 focus:border-zinc-500 block p-2.5"
+              onChange={handleSelectTransactions}
+            >
+              <option value="todas">Todas</option>
+              <option value="cashIn">Cash in</option>
+              <option value="cashOut">Cash out</option>
+            </select>
+          </div>
+          <table className="border-collapse w-[100vw] text-center md:w-[350px] lg:w-[450px] lg:max-w-xl">
+            <thead>
+              <tr className="border-b-2 bg-gradient-to-b from-gray-400/30 to-gray-600/30 text-lg">
+                <th className="border-r-[1px]  p-2">Transação</th>
+                <th className="border-r-[1px]  p-2">Valor</th>
+                <th className="p-2">Data</th>
+              </tr>
+            </thead>
+            <tbody>
+              {transactions?.map((transaction) => (
+                <tr
+                  key={transaction.id}
+                  className="border-b-[1px] hover:bg-gray-500"
+                >
+                  <td className="border-r-[1px] font-bold p-2">
+                    {userLogged?.accountId === transaction.debitedAccountId ? 'Cash out' : 'Cash in'}
+                  </td>
+                  <td className={`${userLogged?.accountId === transaction.debitedAccountId ? 'text-red-500' : 'text-green-500'} border-r-[1px] text-lg font-bold p-2`}>
+                    {`${userLogged?.accountId === transaction.debitedAccountId ?
+                        `- R$ ${transaction.value.toFixed(2)}` :
+                        ` R$ ${transaction.value.toFixed(2)}`}`}
+                  </td>
+                  <td className="p-2">
+                    {(transaction.createdAt.slice(0, 10).split('-').reverse().join('/'))}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </section>
+        <section>
+          <form onSubmit={handleSubmit}>
+            <div className="flex flex-col justify-between items-center w-[100vw] mb-5 md:w-[350px] lg:w-[450px]">
+              <h1 className="text-xl align-middle font-bold my-5">Realizar Transferência</h1>
+              <div className="flex w-full max-w-xl">
+                <div className="flex flex-col items-center w-1/2">
+                  <label className="block text-gray-200 text-sm font-bold mb-2" htmlFor="valor">
+                    Valor
+                  </label>
+                  <input
+                    id="valor"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-zinc-400 focus:border-zinc-500 p-3 w-40"
+                    type="number"
+                    onChange={
+                      (e: React.FormEvent<HTMLInputElement>) =>
+                        setCreditedValue(Number(e.currentTarget.value))
+                    }
+                  />
+                </div>
+                <div className="flex flex-col items-center w-1/2">
+                  <label className="block text-gray-200 text-sm font-bold mb-2" htmlFor="para">
+                    Para:
+                  </label>
+                  <select
+                    id="para"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-zinc-400 focus:border-zinc-500 block p-3 w-40"
+                    onChange={handleSelectCredited}
+                    >
+                    <option></option>
+                    {
+                      allUSers?.map((user) => {
+                        if (user.accountId !== userLogged?.accountId)
+                          return <option key={user.accountId}>{user.username}</option>;
+                      })
+                    }
+                  </select>
+                </div>
+              </div>
+              <button
+              className="bg-gradient-to-br from-gray-400 to-gray-600 text-white font-bold px-4 py-2 rounded hover:bg-gradient-to-br hover:from-gray-500 hover:to-gray-700 my-5 w-24"
+              type="submit"
+              >
+                Ok
+              </button>
+            </div>
+          </form>
+        </section>
+      </main>
+    </div>
   )
 }
 
