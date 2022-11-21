cb492386c2101f8da76590e6adc4727e07b9f675
Thales Maciel <thalesmsm12@gmail.com>
2022-11-21 12:51:36 -0300

    chore: add initial users table data

--- backend/src/database/seeders/20211205212238-user.js
+++ backend/src/database/seeders/20211205212238-user.js
@@ -2,10 +2,22 @@ module.exports = {
   up: async (queryInterface) => {
     await queryInterface.bulkInsert('users', [
       {
-        username: 'User',
-        password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO',
+        username: 'User1',
+        password: '$2a$10$.94vNa/vu2TvtDwfhjUF1u76BKrouu4A9EizN/Y/ebD2OdVmjTCyC',
         accountId: 1
-          // senha: secret_user
+          // senha: secret_User1
+      },
+      {
+        username: 'User2',
+        password: '$2a$10$l729CAXugzoQ5evWIV9LweNpusDE3yYvVy07wKmy4ZCgDnXYHTMVa',
+        accountId: 2
+          // senha: secret_User2
+      },
+      {
+        username: 'User3',
+        password: '$2a$10$wFmdxSP2CF6dct1KP/AMcO0UbosFXdXoahQWfyVBew/rrYPH3k9Sq',
+        accountId: 3
+          // senha: secret_User3
       },
     ], {});
   },
