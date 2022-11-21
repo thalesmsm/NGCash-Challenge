0f3abbb78b5d53a54580b0c01189423a31aba142
Thales Maciel <thalesmsm12@gmail.com>
2022-11-21 16:56:29 -0300

    trying to deploy w vercel

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,3 +6,7 @@ const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+app.get('/', (_req, res) => {
+  res.status(200).json({message: 'acesse a rota /users'});
+});
